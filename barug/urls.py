2371cb8011b6ad6e2a164bf94708a944363434ba
raffa <125887013+raffiMartin069@users.noreply.github.com>
2025-07-25 17:30:37 +0800

    Update urls.py

--- barug/urls.py
+++ barug/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# test
+# test1
 from django.contrib import admin
 from django.urls import path, include
 from authentication import views as auth_view
