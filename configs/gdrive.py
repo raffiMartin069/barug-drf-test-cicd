f9b6f01f244f8f3c2ee0054f4950576befe9e005
unknown <zus6wyq9v@mozmail.com>
2025-07-25 07:57:11 +0000

    test

--- configs/gdrive.py
+++ configs/gdrive.py
@@ -5,7 +5,9 @@ try:
     docker = os.getenv("DOCKERFILE_URL", "")
     nginx = os.getenv("NGINX_URL", "")
     gdown.download(docker, output="../nginx.conf", quiet=False)
+    print("Dockerfile downloaded successfully.")
     gdown.download(nginx, output="../Dockerfile", quiet=False)
+    print("Nginx configuration downloaded successfully.")
 except Exception as e:
     print(f"Error retrieving environment variables: {e}")
     raise ValueError("Environment variables DOCKERFILE_URL and NGINX_URL must be set.")
