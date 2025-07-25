6b2787855df49dfc54a01c31faa40e2ae76b00d9
raffa <125887013+raffiMartin069@users.noreply.github.com>
2025-07-25 17:09:10 +0800

    Update settings.py

--- barug/settings.py
+++ barug/settings.py
@@ -172,6 +172,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "resident/static/resident"),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
